8effe1c0afb100e4dcbf49d9843945b456a98418
kumartul <kumartul001@gmail.com>
2022-02-16 20:30:28 +0530

    [Build]: MVP

---
+++ Chat App/public/js/client.js
@@ -0,0 +1,68 @@
+const socket = io('http://localhost:3000');
+
+// Form Elements
+const form = document.getElementById('form-group');
+const input = form.querySelector('input');
+
+// Message Container
+const msgContainer = document.querySelector('.messages');
+
+// Global Variable
+let name = "";
+
+// Function: Asks the name from the user and sets it to the global variable 'name'
+const askName = () => {
+    let input = prompt("Please enter your name");
+    if(input) {
+        name = input;
+        socket.emit('new-user-joined', name);
+    }
+    else {
+        askName();
+    }
+}
+
+// Function: Appends the message element to the msgContainer
+const append = (message, position) => {
+    const msgElement = document.createElement('div');
+    msgElement.className = "message";
+    msgElement.classList.add(position);
+
+    msgElement.innerHTML = message;
+
+    msgContainer.appendChild(msgElement);
+}
+
+// Add a 'submit' event listener to the form
+form.addEventListener('submit', event => {
+    // Prevent the page from reloading
+    event.preventDefault();
+
+    const message = input.value;
+
+    // Broadcast the message only if the message is not empty
+    if(message !== "") {
+        append(`You: ${message}`, 'right');
+
+        socket.emit('send', message);
+    }
+
+    // Clear the input field and focus on it
+    input.value = "";
+    input.focus();
+});
+
+socket.on('user-joined', name => {
+    append(`<strong>${name}</strong> joined the chat`, 'center');
+});
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+});
+
+socket.on('left', name => {
+    append(`${name} left the chat`, 'center');
+});
+
+// Ask the name of the user as soon as the user visits the website
+askName();
---
+++ Chat App/server/app.js
@@ -0,0 +1,32 @@
+const port = 3000;
+
+const io = require('socket.io')(port, {
+    cors: {
+        origin: '*'
+    }
+});
+
+// Connected users
+const users = {}
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        console.log("sent...");
+        socket.broadcast.emit('receive', { 
+            message, 
+            name: users[socket.id] 
+        });
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        
+        delete users[socket.id];
+    });
+});
