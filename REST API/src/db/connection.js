8753d8f603828d4182fcf49d8f85dbb812054729
kumartul <kumartul001@gmail.com>
2022-02-24 17:50:27 +0530

    [Hide]: The database name

--- REST API/src/db/connection.js
+++ REST API/src/db/connection.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`)
 .then(() => {
     console.log('Connection established successfully...');
 })
