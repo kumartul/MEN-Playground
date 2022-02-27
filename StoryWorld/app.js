1237f453588f9e0a940dc56e2f2e503d5604bf6f
kumartul <kumartul001@gmail.com>
2022-02-27 16:52:26 +0530

    [Add]: PORT

---
+++ StoryWorld/app.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, 'localhost', () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
